1318a56a68e5f6739e622e2530426ae1dfe99661
tony <tonyofbyteball@users.noreply.github.com>
2020-09-28 03:39:21 +0300

    support several arb AAs in one bot

--- arb.js
+++ arb.js
@@ -12,27 +12,36 @@ const operator = require('aabot/operator.js');
 const aa_state = require('aabot/aa_state.js');
 const CurveAA = require('./curve.js');
 
-let curve_aa;
-
 let curves = {};
-let buffers = {};
+let curvesByArb = {};
+let arbsByCurve = {};
+let curvesByBuffer = {};
+
+if (conf.arb_aa && !conf.arb_aas)
+	conf.arb_aas = [conf.arb_aa];
 
 async function addCurve(curve_aa) {
 	const curveAA = await CurveAA.create(curve_aa);
 	curves[curve_aa] = curveAA;
 }
 
-async function addBuffer(address) {
-	buffers[address] = curve_aa;
+async function addBuffer(address, curve_aa) {
+	curvesByBuffer[address] = curve_aa;
 //	await aa_addresses.readAADefinitions([address]);
 //	walletGeneral.addWatchedAddress(curve_aa);
 //	network.addLightWatchedAa(address);
 	await aa_state.followAA(address);
 }
 
-async function estimateAndArb() {
+async function estimateAndArbAll() {
+	for (let arb_aa of conf.arb_aas)
+		await estimateAndArb(arb_aa);
+}
+
+async function estimateAndArb(arb_aa) {
 	const unlock = await mutex.lock('estimate');
 	console.log('===== estimateAndArb');
+	const curve_aa = curvesByArb[arb_aa];
 	const finish = msg => {
 		console.log(msg);
 		unlock();
@@ -48,7 +57,7 @@ async function estimateAndArb() {
 			{
 				app: 'payment',
 				payload: {
-					outputs: [{ address: conf.arb_aa, amount: 1e4 }]
+					outputs: [{ address: arb_aa, amount: 1e4 }]
 				}
 			},
 			{
@@ -61,7 +70,7 @@ async function estimateAndArb() {
 	};
 	let curveAA = curves[curve_aa];
 	await curveAA.updateDataFeeds(false, true);
-	let arrResponses = await aa_composer.estimatePrimaryAATrigger(objUnit, conf.arb_aa, upcomingStateVars, upcomingBalances);
+	let arrResponses = await aa_composer.estimatePrimaryAATrigger(objUnit, arb_aa, upcomingStateVars, upcomingBalances);
 	console.log(`--- estimated responses to simulated arb request`, JSON.stringify(arrResponses, null, 2));
 	aa_unlock();
 	if (arrResponses[0].bounced)
@@ -69,13 +78,13 @@ async function estimateAndArb() {
 	const reserve_delta = arrResponses[0].updatedStateVars[curve_aa].reserve.delta;
 	if (Math.abs(reserve_delta) < conf.min_reserve_delta)
 		return finish(`too small reserve delta: ` + reserve_delta);
-	const unit = await dag.sendAARequest(conf.arb_aa, { arb: 1 });
+	const unit = await dag.sendAARequest(arb_aa, { arb: 1 });
 	if (!unit)
 		return finish(`sending arb request failed`);
 	const objJoint = await dag.readJoint(unit);
 	// upcoming state vars are updated and the next request will see them
 	console.log(`estimateAndArb: calling onAARequest manually`);
-	await aa_state.onAARequest({ unit: objJoint.unit, aa_address: conf.arb_aa });
+	await aa_state.onAARequest({ unit: objJoint.unit, aa_address: arb_aa });
 	unlock();
 }
 
@@ -85,13 +94,15 @@ function onAAResponse(objAAResponse) {
 	if (objAAResponse.bounced && objAAResponse.trigger_address === operator.getAddress())
 		return console.log(`=== our request ${objAAResponse.trigger_unit} bounced with error`, objAAResponse.response.error);
 	const aa_address = objAAResponse.aa_address;
-	if (aa_address === curve_aa)
+	if (curves[aa_address]) {
+		const curve_aa = aa_address;
 		console.log(`new response from the curve ${curve_aa}, will check for arb opportunities`);
-	else if (buffers[aa_address] === curve_aa)
-		return console.log(`new response from buffer ${aa_address} on the curve ${curve_aa}, will skip`);
+		estimateAndArb(arbsByCurve[curve_aa]);
+	}
+	else if (curvesByBuffer[aa_address])
+		return console.log(`new response from buffer ${aa_address} on the curve ${curvesByBuffer[aa_address]}, will skip`);
 	else
 		return console.log(`new response from unknown AA ${aa_address}`);
-	estimateAndArb();
 }
 
 function onAARequest(objAARequest) {
@@ -99,46 +110,61 @@ function onAARequest(objAARequest) {
 	if (objAARequest.unit.authors[0].address === operator.getAddress())
 		return console.log(`skipping our own request`);
 	const aa_address = objAARequest.aa_address;
-	if (aa_address === curve_aa)
+	if (curves[aa_address]) {
+		const curve_aa = aa_address;
 		console.log(`new request to the curve ${curve_aa}, will check for arb opportunities`);
-	else if (buffers[aa_address] === curve_aa) {
+		estimateAndArb(arbsByCurve[curve_aa]);
+	}
+	else if (curvesByBuffer[aa_address]) {
+		const curve_aa = curvesByBuffer[aa_address];
 		const objUnit = objAARequest.unit;
 		const dataMessage = objUnit.messages.find(m => m.app === 'data');
-		if (dataMessage && dataMessage.payload.execute)
+		if (dataMessage && dataMessage.payload.execute) {
 			console.log(`new request to buffer ${aa_address} on the curve ${curve_aa}, will check for arb opportunities`);
+			estimateAndArb(arbsByCurve[curve_aa]);
+		}
 		else
 			return console.log(`new request to deposit funds to buffer ${aa_address} on the curve ${curve_aa}, will skip`);
 	}
 	else
 		return console.log(`new request to unknown AA ${aa_address}`);
-	estimateAndArb();
 }
 
 
 async function startWatching() {
-	await aa_state.followAA(conf.arb_aa);
+	for (let arb_aa of conf.arb_aas) {
+		await aa_state.followAA(arb_aa);
 	
-	// init the curve
-	const params = await dag.readAAParams(conf.arb_aa);
-	curve_aa = params.curve_aa;
-	await addCurve(curve_aa);
-	
+		// init the curve
+		const params = await dag.readAAParams(arb_aa);
+		const curve_aa = params.curve_aa;
+		curvesByArb[arb_aa] = curve_aa;
+		if (arbsByCurve[curve_aa])
+			throw Error(`curve ${curve_aa} is already arbed`);
+		arbsByCurve[curve_aa] = arb_aa;
+		await addCurve(curve_aa);
+	}
+
 	// init the buffers linked to this curve
 	await dag.loadAA(conf.buffer_base_aa);
 	network.addLightWatchedAa(conf.buffer_base_aa); // to learn when new buffer AAs are defined based on it
 	const rows = await dag.getAAsByBaseAAs([conf.buffer_base_aa]);
 	for (let row of rows) {
-		if (row.definition[1].params.curve_aa !== curve_aa)
-			continue;
-		await addBuffer(row.address);
+		let curve_aa = row.definition[1].params.curve_aa;
+		if (curves[curve_aa])
+			await addBuffer(row.address, curve_aa);
 	}
 
 	eventBus.on("aa_request_applied", onAARequest);
 	eventBus.on("aa_response_applied", onAAResponse);
-	eventBus.on("aa_definition_applied-" + conf.buffer_base_aa, addBuffer);
-	eventBus.on('data_feeds_updated', estimateAndArb);
+	eventBus.on("aa_definition_applied-" + conf.buffer_base_aa, async (address, definition) => {
+		let curve_aa = definition[1].params.curve_aa;
+		if (curves[curve_aa])
+			await addBuffer(address, curve_aa);
+	});
+	eventBus.on('data_feeds_updated', estimateAndArbAll);
 
-	setTimeout(estimateAndArb, 1000);
+	setTimeout(estimateAndArbAll, 1000);
 }
 
 
